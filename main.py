150f479cd998b233e3801c02c559f2f5716b5056
mena221166 <menaesam2211@gmail.com>
2023-09-12 18:15:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
 class person :
     name = 'mena'
     age = 17
- def first_person(self):
-    print ( person)
+    
+ def is_adult():
+     if person >= 18:
+      print("you have too much responsibilit ")
+     else:
+       print(" lucky you ")
+       
+print(person.name)
+print(person.age)
+
+
+
+class person1:
+   def __init__(self, name, age):
+      self.name =name
+      self.age =age
+   def __str__(self):
+      return f"my name is {self.name} and uam {self.age} years old"
+   
+first_person= person("manal,22")
+print(first_person)
+first_person.is_adult()
+
+    
+
