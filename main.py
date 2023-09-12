501c38b638ddb081e3b8571d85e28e6eee423098
mena221166 <menaesam2211@gmail.com>
2023-09-12 18:28:50 +0300

    done

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ class person :
     name = 'mena'
     age = 17
     
- def is_adult():
-     if person >= 18:
+    def is_adult():
+     if person.age >= 18:
       print("you have too much responsibilit ")
      else:
        print(" lucky you ")
        
 print(person.name)
 print(person.age)
-
+person.is_adult()
 
 
 class person1:
@@ -20,9 +20,5 @@ class person1:
    def __str__(self):
       return f"my name is {self.name} and uam {self.age} years old"
    
-first_person= person("manal,22")
+first_person= person("manal",22)
 print(first_person)
-first_person.is_adult()
-
-    
-
